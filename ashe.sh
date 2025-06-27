d1e870590878c1e75a040a56fd8477410fbad696
AndrewChacon <andrew.b.chacon@gmail.com>
2025-06-27 11:34:13 -0400

    restructuring to save tool outputs into groups based on their goal

--- ashe.sh
+++ ashe.sh
@@ -12,35 +12,42 @@ TARGET="$2"
 BASE_DIR="./$PROJECT_NAME"
 WORDLIST="/home/drew/tools/SecLists/Discovery/Web-Content/directory-list-2.3-medium.txt"
 
-TOOLS=("nmap" "gobuster" "whatweb" "nikto" "ffuf" "sslscan")
 
-# create folders
-echo "[+] Creating base folder: $BASE_DIR"
-mkdir -p "$BASE_DIR"
+# creating folders
+echo "[+] Creating folder structure under: $BASE_DIR"
+mkdir -p "$BASE_DIR/port_scans"
+mkdir -p "$BASE_DIR/web_fuzzing"
+mkdir -p "$BASE_DIR/fingerprinting"
 
-for TOOL in "${TOOLS[@]}"; do
-  mkdir -p "$BASE_DIR/$TOOL"
-done
 
 # running tools
 echo "[+] Running nmap..."
-nmap -Pn -sV -oN "$BASE_DIR/nmap/nmap.txt" "$TARGET" &
+nmap -Pn "$TARGET" > "$BASE_DIR/port_scans/nmap.txt" &
+# nmap -Pn -sV -oN "$BASE_DIR/port_scans/nmap.txt" "$TARGET" &
 
 echo "[+] Running gobuster..."
-gobuster dir -u "http://$TARGET" -w "$WORDLIST" --no-error -o "$BASE_DIR/gobuster/gobuster.txt" &
-
-echo "[+] Running whatweb..."
-whatweb "$TARGET" > "$BASE_DIR/whatweb/whatweb.txt" &
-
-echo "[+] Running nikto..."
-nikto -h "$TARGET" -output "$BASE_DIR/nikto/nikto.txt" &
+gobuster dir -u "http://$TARGET" -w "$WORDLIST" -t 50 --no-error -o "$BASE_DIR/web_fuzzing/gobuster.txt" &
 
 echo "[+] Running ffuf..."
-ffuf -u "http://$TARGET/FUZZ" -w "$WORDLIST" -of html -o "$BASE_DIR/ffuf/ffuf.html" -fc 404 &
+ffuf -u "http://$TARGET/FUZZ" -w "$WORDLIST" -t 50 -fc 404 > "$BASE_DIR/web_fuzzing/ffuf.txt" &
 
-echo "[+] Running sslscan..."
-sslscan --no-colour "$TARGET" > "$BASE_DIR/sslscan/sslscan.txt" &
+echo "[+] Running whatweb..."
+whatweb "$TARGET" > "$BASE_DIR/fingerprinting/whatweb.txt" &
+
+echo "[+] Running nikto..."
+nikto -h "$TARGET" -output "$BASE_DIR/fingerprinting/nikto.txt" &
 
 # wait for everything to finish running
 wait
 echo "[+] All recon tools finished. Output saved in: $BASE_DIR"
+
+
+
+# restructure plan
+# -----------------------
+# nmap scans - normal scan, deep scan, scan all ports, scan UDP ports
+# fuzzing - gobuster, ffuf
+# fingerprinting - whatweb
+# find all subdomains - active domains, screenshots
+
+# research more recon/enumeration tools
